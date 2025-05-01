3168e7d80d239fe904239a00075e049e1aa8e511
TaranHothi <hothi@mines.edu>
2025-05-01 00:23:07 -0600

    Update server.c

--- server.c
+++ server.c
@@ -4,51 +4,50 @@
 #include <fcntl.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/stat.h>
 
 struct message {
-	char source[50];
-	char target[50]; 
-	char msg[200]; // message body
+    char source[50];
+    char target[50]; 
+    char msg[200];
 };
 
 void terminate(int sig) {
-	printf("Exiting....\n");
-	fflush(stdout);
-	exit(0);
+    printf("Exiting....\n");
+    fflush(stdout);
+    exit(0);
 }
 
 int main() {
-	int server;
-	int target;
-	int dummyfd;
-	struct message req;
-	signal(SIGPIPE,SIG_IGN);
-	signal(SIGINT,terminate);
-	server = open("serverFIFO",O_RDONLY);
-	dummyfd = open("serverFIFO",O_WRONLY);
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGINT, terminate);
+    
+    //Create server FIFO if it doesn't exist
+    mkfifo("serverFIFO", 0666);
+    
+    //Open FIFOs with non-blocking flags
+    int server_fd = open("serverFIFO", O_RDONLY | O_NONBLOCK);
+    int dummy_fd = open("serverFIFO", O_WRONLY | O_NONBLOCK);
 
-	while (1) {
-		// TODO:
-		// read requests from serverFIFO
-		ssize_t bytes = read(server, &req, sizeof(req));
-		if (bytes != sizeof(req)) continue;
+    while(1) {
+        struct message req;
+        ssize_t bytes = read(server_fd, &req, sizeof(req));
+        
+        //Only process full messages
+        if(bytes == sizeof(req)) {
+            //Forwards message to target user
+            int target_fd = open(req.target, O_WRONLY | O_NONBLOCK);
+            if(target_fd != -1) {
+                write(target_fd, &req, sizeof(req));
+                close(target_fd);
+            }
+        }
+        
+        //Prevents CPU hogging
+        usleep(100000);
+    }
 
-		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
-
-		// TODO:
-		// open target FIFO and write the whole message struct to the target FIFO
-		// close target FIFO after writing the message
-		int target_fd = open(req.target, O_WRONLY);
-		if (target_fd == -1) {
-			perror("server: open target FIFO failed");
-			continue;
-		}
-		if (write(target_fd, &req, sizeof(req)) == -1) {
-			perror("server: write failed");
-		}
-		close(target_fd);
-	}
-	close(server);
-	close(dummyfd);
-	return 0;
+    close(server_fd);
+    close(dummy_fd);
+    return 0;
 }
